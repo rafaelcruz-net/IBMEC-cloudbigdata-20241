93372441f942b9de552285d20c78ef30227a8632
Rafael Cruz <rafaelcruz.net81@gmail.com>
2024-03-06 08:47:35 -0300

    Criação do projeto

---
+++ src/main/java/br/com/ibmec/cloud/cargallery/CargalleryApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibmec.cloud.cargallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ibmec/cloud/cargallery/CargalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibmec.cloud.cargallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
